9065f176d33677c56c0a8f0e7e68c478c3b8f42e
DomciaiOliwcia <ddrozdowska2@st.swps.edu.pl>
2024-06-29 16:05:31 +0200

    Update skrzynka3.js

--- skrzynka3.js
+++ skrzynka3.js
@@ -110,7 +110,7 @@ function drawStartButton() {
   // Zmiana koloru przycisku po kliknięciu
   if (buttonClicked) {
     fill(colors.purple);
-    window.location.href = ''https://tradski.github.io/Sekrety/'';
+    window.location.href = 'https://tradski.github.io/Sekrety/' ;
   } else if (buttonHovered) {
     fill(colors.darkPink);
   } else {
